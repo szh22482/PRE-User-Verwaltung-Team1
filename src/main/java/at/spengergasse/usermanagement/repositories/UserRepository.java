b2afc4525762c646d22b1b6a6e4cf8108cbae415
wac22585 <wac22585@spengergasse.at>
2023-12-04 11:33:52 +0100

    reset project

--- src/main/java/at/spengergasse/usermanagement/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package at.spengergasse.usermanagement.repositories;
-
-import at.spengergasse.usermanagement.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
