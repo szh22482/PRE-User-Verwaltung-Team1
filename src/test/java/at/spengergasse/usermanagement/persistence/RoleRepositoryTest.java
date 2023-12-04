c9b427d0e2ae07875804913150b737cd726b6012
wac22585 <wac22585@spengergasse.at>
2023-12-04 12:03:56 +0100

    created tests for services

---
+++ src/test/java/at/spengergasse/usermanagement/persistence/RoleRepositoryTest.java
@@ -0,0 +1,24 @@
+package at.spengergasse.usermanagement.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByRole() {
+    }
+}
---
+++ src/test/java/at/spengergasse/usermanagement/persistence/UserRepositoryTest.java
@@ -0,0 +1,24 @@
+package at.spengergasse.usermanagement.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByName() {
+    }
+}
