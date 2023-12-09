1dd40f830b03bd7f9b6f66d5d4687d9a914b57fd
wac22585 <wac22585@spengergasse.at>
2023-12-10 00:25:06 +0100

    added database connection and login endpoint

--- backend/src/main/java/at/spengergasse/backend/controller/UserController.java
+++ backend/src/main/java/at/spengergasse/backend/controller/UserController.java
@@ -1,8 +1,11 @@
 package at.spengergasse.backend.controller;
 
+import at.spengergasse.backend.dto.UserDto;
 import at.spengergasse.backend.model.User;
 import at.spengergasse.backend.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,5 +20,31 @@ public class UserController
     public @ResponseBody Iterable<User> allUsers()
     {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/login")
+    public boolean login(@RequestParam("email") String email, @RequestParam("password") String password)
+    {
+        if(email == null || password == null)
+        {
+            return false;
+        }
+
+        User user = userRepository.findByEmail(email);
+        return user.getPassword().equals(password);
+    }
+
+    @PostMapping("/add")
+    public @ResponseStatus ResponseEntity createUser(@RequestBody User user)
+    {
+        try
+        {
+            userRepository.save(user);
+            return new ResponseEntity("OK", HttpStatus.CREATED);
+        } catch (Exception e)
+        {
+            return new ResponseEntity("Error", HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
