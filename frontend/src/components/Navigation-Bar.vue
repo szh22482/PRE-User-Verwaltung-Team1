2baa31d438df9fb2ec4e92b3f5e28828ac523485
wac22585 <wac22585@spengergasse.at>
2024-01-14 20:31:24 +0100

    Navigation Bar for HomePage and Account

---
+++ frontend/src/components/Navigation-Bar.vue
@@ -0,0 +1,124 @@
+<template>
+    <v-btn class="toggle-btn" v-if="!drawer && isSmallScreen" @click="toggleDrawer" variant="plain" icon="mdi-menu" :ripple="false"/> 
+    <div v-if="isSmallScreen && drawer" :style="{'margin-right': '48px'}"></div>
+    <div :style="{ 'margin-right': marginWidth }">
+      <v-navigation-drawer width="200" v-model="drawer">
+        <v-btn class="close-btn" v-if="isSmallScreen" @click="toggleDrawer" variant="plain" icon="mdi-close" :ripple="false" /> 
+        <v-list-item class="nav-item first-item">
+          <router-link class="router-link" v-bind:to="`/account`">
+            <v-btn prepend-icon="mdi-cog" :ripple="false" variant="text" size="large"><span class="font-weight-bold">ACCOUNT</span></v-btn>
+          </router-link>
+          
+        </v-list-item>
+        <v-list-item v-if="isAdmin" class="nav-item">
+          <router-link class="router-link" v-bind:to="`/home`">
+            <v-btn prepend-icon="mdi-account-multiple" :ripple="false" variant="text" size="large"><span class="font-weight-bold">USERS</span></v-btn>
+          </router-link>
+        </v-list-item>
+        <v-list-item v-if="isAdmin" class="nav-item">
+          <router-link class="router-link" v-bind:to="`/add`">
+            <v-btn prepend-icon="mdi-plus" :ripple="false" variant="text" size="large"><span class="font-weight-bold">ADD USER</span></v-btn>
+          </router-link>
+        </v-list-item>
+        <v-list-item class="logout">
+          <v-btn prepend-icon="mdi-logout" :ripple="false" variant="text" size="large" @click="logout()">Log out</v-btn>
+        </v-list-item>
+      </v-navigation-drawer>
+    </div>
+</template>
+
+<script setup>
+  import axios from 'axios';
+</script>
+
+<script>
+    export default {
+        data() {
+            return {
+                drawer: true,
+                isSmallScreen: false,
+                marginWidth: '70px',
+                isAdmin: false,
+            }
+        },
+        async mounted() {
+          this.checkScreenSize();
+          window.addEventListener('resize', this.checkScreenSize)
+          if(this.isSmallScreen) this.drawer = false;
+          try {
+            console.log(localStorage.getItem('id'))
+              const response = await axios.get(`users/get/${localStorage.getItem('id')}`);
+              if(response.status == 200) {
+                  if(response.data.roles.includes('ADMINISTRATOR')) this.isAdmin = true;
+              }
+              else {
+                console.log("An error occured")
+              }
+          } catch(error) {
+              alert(error)
+              console.log(error)
+          }
+        },
+        beforeUnmount() {
+            window.removeEventListener('resize', this.checkScreenSize);
+        },
+        methods: {
+          toggleDrawer() {
+              this.drawer = !this.drawer;
+          },
+          checkScreenSize() {
+              this.isSmallScreen = window.innerWidth < 1280;
+              this.marginWidth = this.isSmallScreen ? '0px' : '70px';
+          },
+          async logout() {
+            try {
+              const response = await axios.delete('/auth/logout', {
+                withCredentials: true
+              });
+
+              if(response.status === 200) {
+                this.$router.push({name: 'Login'});
+              } else {
+                 alert('Error deleting user:', response.data);
+              }
+            } catch(error) {
+              alert('An error occurred during logout');
+            }
+          },
+        }
+    }
+</script>
+
+<style>
+    .toggle-btn {
+        position: relative;
+        left: 35px;
+        top: 20px;
+    }
+
+    .close-btn {
+        margin-top: 20px;
+        position: relative;
+        left: 140px;
+        margin-bottom: -50px;
+    }
+
+    .first-item {
+        margin-top: 70px !important;
+    }
+
+    .nav-item {
+        margin-block: 10px;
+    }
+
+    .logout {
+        position: fixed !important;
+        bottom: 40px !important;
+        z-index: 10;
+    }
+
+    .router-link {
+      text-decoration: none !important;
+      color: #212121;
+    }
+</style>
